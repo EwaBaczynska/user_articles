99ed13513ddee15a6a87e5e39dd100dd3de03db5
EwaBaczynska <baczynska.ewa@gmail.com>
2022-11-04 19:51:49 +0100

    Add Freezed (auto jsonSerializable)

--- lib/data/remote_data_sources/articles_remote_data_source.dart
+++ lib/data/remote_data_sources/articles_remote_data_source.dart
@@ -22,7 +22,7 @@ class ArticlesMockedDataSource {
 class ArticlesRemoteDioDataSource {
   Future<List<Map<String, dynamic>>?> getArticles() async {
     final response = await Dio().get<List<dynamic>>(
-        'https://my-json-server.typicode.com/adamsmaka/json-demo/articles');
+        'http://my-json-server.typicode.com/adamsmaka/json-demo/articles');
     final listDynamic = response.data;
     if (listDynamic == null) {
       return null;
